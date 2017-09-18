b6c783262a4556d4ad4becf2ca40fab348e914ab
Freddy LAZO <flazo@anda.pe>
2017-09-18 12:20:33 -0500

    Initial commit test

---
+++ app/src/main/java/com/example/anda_pc/gittest/Test.java
@@ -0,0 +1,12 @@
+package com.example.anda_pc.gittest;
+
+import android.os.Bundle;
+
+public class Test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+    }
+}
